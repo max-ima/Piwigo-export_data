c648cc6b533d5295d2444b958a77e7ade825a43d
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-01 18:24:42 +0100

    missing

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -3,4 +3,5 @@ $lang['Export Data'] = 'Export Data';
 $lang['Export albums'] = 'Export albums';
 $lang['Export photos'] = 'Export photos';
 $lang['Export comments'] = 'Export comments';
+$lang['Export downloads'] = 'Export downloads';
 ?>
